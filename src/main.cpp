61d0827c693d3ec0fb936d85937eaede4b96ef0e
Eduardo Brito Labrada <eblabrada@gmail.com>
2026-06-28 20:18:57 -0400

    feat: add basic commands to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
+#include <iostream>
+
 #include "all"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) { 
     
-    return 0;
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <command> [args...]" << std::endl;
+        return 1;
+    }
+
+    std::string command = argv[1];
+    
+    if (command == "task") {
+        if (argc < 4) {
+            std::cerr << "Usage: " << argv[0] << " task <code> <name>" << std::endl;
+            return 1;
+        }
+        std::string code = argv[2];
+        std::string name = argv[3];
+        ops::task::Task task(code, name);
+        if (!task.create_task_folder()) {
+            return 1;
+        }
+    } 
+
+    return 0; 
 }
